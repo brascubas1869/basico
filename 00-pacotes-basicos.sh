a06b7c5ae03c052b4e1087bb342e141ef4ef281f
brascubas1869 <97002733+brascubas1869@users.noreply.github.com>
2022-01-21 21:44:56 -0300

    Add files via upload

---
+++ 00-pacotes-basicos.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+sudo apt install build-essential \
+                 apt-listchanges \
+                 apt-listbugs \
+                 curl \
+                 lynx \
+                 i3 \
+                 xorg \
+                 lightdm \
+                 lxappearance \
+                 feh \
+                 pcmanfm \
+                 qutebrowser \
+                 pulseaudio \
+                 alsa-utils \
+                 pavucontrol \
+                 terminator \
+                 firmware-atheros \
+                 firmware-realtek \
+                 rofi \
+                 vim \
+                 picom \
+                 network-manager \
+                 blueman \
+                 htop \
+                 papirus-icon-theme \
+                 arc-theme \
+                 youtube-dl \
+                 gparted \
+                 vlc \
+                 geany
