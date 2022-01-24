d3678114607f544d73203013bb994c9ed3cc3313
brascubas1869 <97002733+brascubas1869@users.noreply.github.com>
2022-01-24 00:17:07 -0300

    Add files via upload

---
+++ 00-i3install.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+sudo apt install build-essential \
+                 apt-listchanges \
+                 apt-listbugs \
+                 curl \
+                 lynx \
+                 i3 \
+                 xorg \
+                 lightdm \
+                 lxappearance \
+                 feh \
+                 pcmanfm \
+                 qutebrowser \
+                 pulseaudio \
+                 alsa-utils \
+                 pavucontrol \
+                 terminator \
+                 firmware-atheros \
+                 firmware-realtek \
+                 rofi \
+                 vim \
+                 picom \
+                 network-manager \
+                 blueman \
+                 htop \
+                 papirus-icon-theme \
+                 arc-theme \
+                 youtube-dl \
+                 gparted \
+                 vlc \
+                 geany
+                 libreoffice \
+                 libreoffice-l10n-pt-br \
+                 libreoffice-gnome \
+                 libreoffice-style-breeze \
+                 goldendict \
+                 font-manager \
+                 vlc \
+                 vlc-l10n \
+                 cal
+
+
+
